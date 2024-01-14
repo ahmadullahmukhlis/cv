686127243a946e6bfcf71c5757b2042bacbaeb2b
unknown <nasarimukhlis@gmail.com>
2024-01-14 08:37:37 -0800

    the index file added successfully

---
+++ index.php
@@ -0,0 +1,440 @@
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mukhlis document official website </title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="font_awsome/all.css">
+    <link rel="stylesheet" href="cv/css/style.css">
+    <link rel="stylesheet" href="css/style.css">  
+    <script src="font_awsome/all.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/custem.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.0.272/jspdf.debug.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- header starting-->
+ <nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-light text-light">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">
+      <img src="upload-img/ahmad.jpg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      muklis docs
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item"><a class="nav-link " href="index.php" >home</a></li>
+      <li class="nav-item"><a class="nav-link " href="#" id="total-cv">cv</a></li>
+      <li class="nav-item"><a class="nav-link" href="#" id="get-start" >creat cv</a></li>
+      <li class="nav-item"><a class="nav-link" href="#" id="word-template" > word cv template </a></li>  
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          recomendation latter
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#" id="creat-latter">creat rlatter</a></li>
+            <li><a class="dropdown-item" href="#" id="download-latter">download latter</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex mb-0 px-4  mx-0">  
+      <button class="btn btn-success" id="logout">logout</button>
+          <div class="dropdown">
+          <button type="button" class="btn btn-success dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+          <i class="far fa-user-circle" style="font-size:35px;"></i>
+          <span style="font-size:25px;" class="p-1">login</span>
+          </button>
+          <ul class="dropdown-menu ">
+          <li><a class="dropdown-item" id="login-modal-btn">login</a></li>
+          <li><a class="dropdown-item" id="register-modal-btn">registration</a></li>
+      </ul>
+      </form>
+    </div>
+  </div>
+</nav>
+<div id="cv-body" class=" my-2">
+  
+<div class="container-fluid">
+  <div class="row">
+    <div class="biner text-center ">
+      <a href="#" class="btn btn-success my-md-5" id="get-start-btn">get starte </a>
+    </div>
+  </div>
+</div>
+<div class="container-fluid">
+<div class="row my-3">
+    <div class="col-md-3 col-sm-6 my-3" id="creative">
+        <div class="product-grid">
+            <div class="product-image" id="creative">
+                <a href="#" class="image">
+                    <img class="pic-1" src="upload-img/creative.jpeg" id="creative">
+                    <img class="pic-2" src="upload-img/creative.jpeg"id="creative">
+                </a>
+                <ul class="product-links">
+                    <li><a href="#" id="creative">download</a></li>
+                    <li><a href="#"id="creative"><i class="fa fa-heart"></i></a></li>
+                    <li><a href="#"id="creative"><i class="fa fa-random"></i></a></li>
+                    <li><a href="#" id="creative"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+                <h3 class="title"><a href="#">creative cv</a></h3>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="col-md-3 col-sm-6 my-3" id="creative2">
+        <div class="product-grid">
+            <div class="product-image">
+                <a href="#" class="image">
+                    <img class="pic-1" src="upload-img/creative2cv.jpeg"id="creative2">
+                    <img class="pic-2" src="upload-img/creative2cv.jpeg"id="creative2">
+                </a>
+                <ul class="product-links">
+                    <li><a href="#"id="creative2">download</a></li>
+                    <li><a href="#"id="creative2"><i class="fa fa-heart"></i></a></li>
+                    <li><a href="#"id="creative2"><i class="fa fa-random"></i></a></li>
+                    <li><a href="#"id="creative2"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+                <h3 class="title"id="creative2"><a href="#">creative 2 cv</a></h3>
+            </div>
+        </div>
+    </div>
+
+    
+    <div class="col-md-3 col-sm-6 my-3">
+        <div class="product-grid">
+            <div class="product-image" id="creative3">
+                <a href="#" class="image">
+                    <img class="pic-1" src="upload-img/creative3cv.jpeg"id="creative3">
+                    <img class="pic-2" src="upload-img/creative3cv.jpeg"id="creative3">
+                </a>
+                <ul class="product-links">
+                    <li><a href="#"id="creative3">download</a></li>
+                    <li><a href="#"id="creative3"><i class="fa fa-heart"></i></a></li>
+                    <li><a href="#" id="creative3"><i class="fa fa-random"></i></a></li>
+                    <li><a href="#"id="creative3"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+            <h3 class="title"><a href="#" id="creative3">creative 3 cv</a></h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-6 my-3">
+        <div class="product-grid">
+            <div class="product-image">
+                <a href="#" class="image" id="info1">
+                    <img class="pic-1" src="upload-img/info1cv.jpeg"id="info1">
+                    <img class="pic-2" src="upload-img/info1cv.jpeg"id="info1">
+                </a>
+                <ul class="product-links">
+                    <li><a href="#"id="info1">download</a></li>
+                    <li><a href="#"id="info1"><i class="fa fa-heart"></i></a></li>
+                    <li><a href="#"id="info1"><i class="fa fa-random"></i></a></li>
+                    <li><a href="#"id="info1"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+                <h3 class="title"><a href="#"id="info1">info 1</a></h3>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row my-3">
+    <div class="col-md-3 col-sm-6 my-3">
+        <div class="product-grid">
+            <div class="product-image">
+                <a href="#" class="image" id="pro3">
+                    <img class="pic-1" src="upload-img/pro3cv.jpeg">
+                    <img class="pic-2" src="upload-img/pro3cv.jpeg">
+                </a>
+                <ul class="product-links">
+                    <li><a href="#"id="pro3">download</a></li>
+                    <li><a href="#"id="pro3"><i class="fa fa-heart"></i></a></li>
+                    <li><a href="#"id="pro3"><i class="fa fa-random"></i></a></li>
+                    <li><a href="#"id="pro3"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+                <h3 class="title"id="pro3"><a href="#">profetional 3 cv</a></h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-6 my-3">
+        <div class="product-grid">
+            <div class="product-image">
+                <a href="#" class="image"id="pro4">
+                    <img class="pic-1" src="upload-img/pro4cv.jpeg">
+                    <img class="pic-2" src="upload-img/pro4cv.jpeg">
+                </a>
+                <ul class="product-links">
+                    <li><a href="#" id="pro4">download</a></li>
+                    <li><a href="#"id="pro4"><i class="fa fa-heart"></i></a></li>
+                    <li><a href="#"id="pro4"><i class="fa fa-random"></i></a></li>
+                    <li><a href="#"id="pro4"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+                <h3 class="title"><a href="#"id="pro4">profetional 4 cv</a></h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-6 my-3">
+        <div class="product-grid">
+            <div class="product-image">
+                <a href="#" class="image" id="pro5">
+                    <img class="pic-1" src="upload-img/profetional2cv.jpeg">
+                    <img class="pic-2" src="upload-img/profetional2cv.jpeg">
+                </a>
+                <ul class="product-links">
+                    <li><a href="#"id="pro5">download</a></li>
+                    <li><a href="#"id="pro5"><i class="fa fa-heart"></i></a></li>
+                    <li><a href="#"id="pro5"><i class="fa fa-random"></i></a></li>
+                    <li><a href="#"id="pro5"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+            <h3 class="title"><a href="#pro5" id="pro5">profetional 2 cv</a></h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-6 my-3">
+        <div class="product-grid">
+            <div class="product-image">
+                <a href="#" class="image" id="pro1">
+                    <img class="pic-1" src="upload-img/profetionalcv.jpeg">
+                    <img class="pic-2" src="upload-img/profetionalcv.jpeg">
+                </a>
+                <ul class="product-links">
+                    <li><a href="#"id="pro1">download</a></li>
+                    <li><a href="#"id="pro1"><i class="fa fa-heart"></i></a></li>
+                    <li><a href="#"id="pro1"><i class="fa fa-random"></i></a></li>
+                    <li><a href="#"id="pro1"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+                <h3 class="title"><a href="#"id="pro1">profetional</a></h3>
+            </div>
+        </div>
+    </div>
+</div>
+</div> 
+<?php
+  include "php/config.php";
+?>
+<div class="container-fluid">
+<div class="row my-3">
+    <?php
+    $sql="SELECT * FROM wordtemplate";
+    $result = mysqli_query($conn,$sql);
+    if(mysqli_num_rows($result) > 0){
+      echo'<h1 class="text-center my-3">download free cv templaet in word </h1>';
+       
+       while($row = mysqli_fetch_assoc($result)){ 
+    ?>
+    <div class="col-md-3 col-sm-6 my-3" id="word" >
+        <div class="product-grid">
+            <div class="product-image">
+                <a href="#" class="image" id="<?php echo $row['id'] ?>" >
+                    <img class="pic-1" src="admin/upload-img/<?php echo $row['imgfile'] ?>" id="<?php echo $row['id'] ?>">
+                    <img class="pic-2" src="admin/upload-img/<?php echo $row['imgfile'] ?>"id="<?php echo $row['id'] ?>">
+                </a>
+                <ul class="product-links">
+                    <li><a href="admin/upload-template/<?php echo $row['wordfile'] ?>"id="<?php echo $row['id'] ?>">download</a></li>
+                    <li><a href="#" id="<?php echo $row['id'] ?>"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+                <h3 class="title"><a href="admin/upload-template/<?php echo $row['wordfile'] ?>" id="<?php echo $row['id'] ?>" download>download</a></h3>
+            </div>
+        </div>
+    </div>
+    <?php }}?>
+</div>
+</div>  
+</div>
+<div class="container-fluid px-0 mx-0" id="footer">
+    <div class="row mx-0 px-0">
+    <footer class="text-center text-white">
+  <!-- Grid container -->
+  <div class="container pt-4">
+    <!-- Section: Social media -->
+    <section class="mb-4 text-white" id="sub-footer">
+      <!-- Facebook -->
+      <a href="https://web.facebook.com/nasarimukhlis.2021"
+        class="btn btn-link btn-floating btn-lg text-white m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="light"
+        ><i class="fab fa-facebook-f"></i
+      ></a>
+
+      <!-- Twitter -->
+      <a href="https://mobile.twitter.com/Ahmadullahmukhl"
+        class="btn btn-link btn-floating btn-lg text-white  m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="white"
+        ><i class="fab fa-twitter"></i
+      ></a>
+
+      <!-- Google -->
+      <a href="https://google.com"
+        class="btn btn-link btn-floating btn-lg text-white  m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-google"></i
+      ></a>
+
+      <!-- Instagram -->
+      <a
+      href="https://www.instagram.com/nasarimukhlis/"
+        class="btn btn-link btn-floating btn-lg text-white  m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="light"
+        ><i class="fab fa-instagram"></i
+      ></a>
+
+      <!-- Linkedin -->
+      <a href="https://www.linkedin.com/in/nasari-mukhlis-0a7604226/"
+        class="btn btn-link btn-floating btn-lg text-white  m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-linkedin"></i
+      ></a>
+      <!-- Github -->
+     
+      <a href="tel:+923489875617"
+        class="btn btn-link btn-floating btn-lg text-white  m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fas fa-phone-volume"></i>
+      </a>
+      <!-- mobile nomber -->
+      <a href="https://account.microsoft.com/profile"
+        class="btn btn-link btn-floating btn-lg text-white  m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-skype"></i>
+      </a>
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center text-white p-3" id="footer">
+    <div class="col-12">
+    
+                <span></span>
+
+    </div >
+     Copyright by :
+    <a class="" href="https://web.facebook.com/nasarimukhlis.2021">Ahmad Ullah Mukhlis.com</a>
+  </div>
+  <!-- Copyright -->
+    </div>
+</div>
+<div class="modal fade" id="log-in" tabindex="-1" aria-labelledby="login" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable">
+    <div class="modal-content bg-success">
+      <div class="modal-header">
+        <h5 class="modal-title" id="log">login </h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form id="login-form">
+          <div class="mb-3">
+            <label for="l-email" class="col-form-label">email</label>
+            <input type="email" class="form-control" id="login-email" name="email">
+          </div>
+          <div class="mb-3">
+            <label for="passowrd" class="col-form-label">password</label>
+            <input type="password" class="form-control" id="login-password" name="password">
+          </div>
+         
+        </form>
+      </div>
+      <div class="modal-footer">
+      <a class="btn btn-link" data-bs-toggle="modal" data-bs-target="#registration" data-bs-dismiss="modal" data-bs-whatever="@mdo">registration</a>
+        <button type="button" class="btn btn-success" data-bs-dismiss="modal">Close</button>
+        <button type="button" type="submit" class="btn btn-success" id="login-btn" name="f-login">login</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- registraion modal form -->
+<div class="modal fade " id="registration" tabindex="-1" aria-labelledby="registraion" aria-hidden="true">
+  <div class="modal-dialog  modal-dialog-scrollable">
+    <div class="modal-content bg-success">
+    <div class="modal-header">
+        <h5 class="modal-title" id="registration">registration </h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form id="registration-form">
+          <div class="mb-3">
+            <label for="name" class="col-form-label">name</label>
+            <input type="text" class="form-control" id="name" name="name">
+          </div>
+          <div class="mb-3">
+            <label for="email" class="col-form-label">email</label>
+            <input type="email" class="form-control" id="email" name="email">
+          </div>
+          <div class="mb-3">
+            <label for="passowrd" class="col-form-label">passwprd</label>
+            <input type="password" class="form-control" id="password" name="password">
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+      <a class="btn btn-link" data-bs-toggle="modal" data-bs-target="#log-in" data-bs-dismiss="modal" data-bs-whatever="@mdo">login</a>
+        <button type="button" class="btn btn-success" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-success" id="register-btn">register</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- success modal box -->
+<div class="modal fade " id="success" tabindex="-1" aria-labelledby="registraion" aria-hidden="true">
+  <div class="modal-dialog  ">
+    <div class="modal-content  bg-success ">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      <div class="modal-body">
+      <h1 class="text-center py-2" style="font-size:100px;"><i class="fas fa-check-circle bg-success"></i></h1>
+      <h3 class="text-center py-2">successfull</h3>
+      <h5 class="text-center py-2" id="success-message"></5>
+      <button type="button" class="btn btn-success" data-bs-dismiss="modal">done</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- warning modal box -->
+<div class="modal fade " id="warrning" tabindex="-1" aria-labelledby="warrning" aria-hidden="true">
+  <div class="modal-dialog   ">
+    <div class="modal-content bg-success  ">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      <div class="modal-body">
+      <h1 class="text-center py-2" style="font-size:100px;"><i class="fas fa-exclamation-triangle"></i></h1>
+      <h3 class="text-center py-2">warrnig</h3>
+      <h5 class="text-center py-2" id="warrning-message"></5>
+      
+      </div>
+    </div>
+  </div>
+</div>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/custem.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
