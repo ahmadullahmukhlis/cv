b03cb60c2f41902afdf78ea4ed0535366d91b9a9
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2024-01-14 09:30:36 -0800

    the peoblem fixed

---
+++ admin/import/header.php
@@ -0,0 +1,46 @@
+    <!-- header starting-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-light" id="header">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">
+      <img src="upload-img/ahmad.jpg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      Bootstrap
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#" id="home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#" id="activities">activities</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#" id="award">award</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#" id="contect">contect</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#" id="education">education</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#" id="experience">experience</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#" id="intrest">intrest</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#" id="objective">objective</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#" id="skill">skill</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#" id="wordcv">word cv</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
