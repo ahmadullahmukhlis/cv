5fd7bd2b0f15316a9e3e6f3cc65d5fbbdcf010e8
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2024-01-14 09:22:40 -0800

    the peoblem fixed

---
+++ cv/total-cv.php
@@ -0,0 +1,455 @@
+<?php
+  include "php/config.php";
+?>
+
+<div class="container-fluid">
+<div class="row my-3">
+    <div class="col-md-3 col-sm-6 my-3" id="creative">
+        <div class="product-grid">
+            <div class="product-image" id="creative">
+                <a href="#" class="image">
+                    <img class="pic-1" src="upload-img/creative.jpeg" id="creative">
+                    <img class="pic-2" src="upload-img/creative.jpeg"id="creative">
+                </a>
+                <ul class="product-links">
+                    <li><a href="#" id="creative">download</a></li>
+                    <li><a href="#"id="creative"><i class="fa fa-heart"></i></a></li>
+                    <li><a href="#"id="creative"><i class="fa fa-random"></i></a></li>
+                    <li><a href="#" id="creative"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+                <h3 class="title"><a href="#">creative cv</a></h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-6 my-3" id="creative2">
+        <div class="product-grid">
+            <div class="product-image">
+                <a href="#" class="image">
+                    <img class="pic-1" src="upload-img/creative2cv.jpeg"id="creative2">
+                    <img class="pic-2" src="upload-img/creative2cv.jpeg"id="creative2">
+                </a>
+                <ul class="product-links">
+                    <li><a href="#"id="creative2">download</a></li>
+                    <li><a href="#"id="creative2"><i class="fa fa-heart"></i></a></li>
+                    <li><a href="#"id="creative2"><i class="fa fa-random"></i></a></li>
+                    <li><a href="#"id="creative2"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+                <h3 class="title"id="creative2"><a href="#">creative 2 cv</a></h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-6 my-3">
+        <div class="product-grid">
+            <div class="product-image" id="creative3">
+                <a href="#" class="image">
+                    <img class="pic-1" src="upload-img/creative3cv.jpeg"id="creative3">
+                    <img class="pic-2" src="upload-img/creative3cv.jpeg"id="creative3">
+                </a>
+                <ul class="product-links">
+                    <li><a href="#"id="creative3">download</a></li>
+                    <li><a href="#"id="creative3"><i class="fa fa-heart"></i></a></li>
+                    <li><a href="#" id="creative3"><i class="fa fa-random"></i></a></li>
+                    <li><a href="#"id="creative3"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+            <h3 class="title"><a href="#" id="creative3">creative 3 cv</a></h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-6 my-3">
+        <div class="product-grid">
+            <div class="product-image">
+                <a href="#" class="image" id="info1">
+                    <img class="pic-1" src="upload-img/info1cv.jpeg"id="info1">
+                    <img class="pic-2" src="upload-img/info1cv.jpeg"id="info1">
+                </a>
+                <ul class="product-links">
+                    <li><a href="#"id="info1">download</a></li>
+                    <li><a href="#"id="info1"><i class="fa fa-heart"></i></a></li>
+                    <li><a href="#"id="info1"><i class="fa fa-random"></i></a></li>
+                    <li><a href="#"id="info1"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+                <h3 class="title"><a href="#"id="info1">info 1</a></h3>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row my-3">
+    <div class="col-md-3 col-sm-6 my-3">
+        <div class="product-grid">
+            <div class="product-image">
+                <a href="#" class="image" id="pro3">
+                    <img class="pic-1" src="upload-img/pro3cv.jpeg">
+                    <img class="pic-2" src="upload-img/pro3cv.jpeg">
+                </a>
+                <ul class="product-links">
+                    <li><a href="#"id="pro3">download</a></li>
+                    <li><a href="#"id="pro3"><i class="fa fa-heart"></i></a></li>
+                    <li><a href="#"id="pro3"><i class="fa fa-random"></i></a></li>
+                    <li><a href="#"id="pro3"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+                <h3 class="title"id="pro3"><a href="#">profetional 3 cv</a></h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-6 my-3">
+        <div class="product-grid">
+            <div class="product-image">
+                <a href="#" class="image"id="pro4">
+                    <img class="pic-1" src="upload-img/pro4cv.jpeg">
+                    <img class="pic-2" src="upload-img/pro4cv.jpeg">
+                </a>
+                <ul class="product-links">
+                    <li><a href="#" id="pro4">download</a></li>
+                    <li><a href="#"id="pro4"><i class="fa fa-heart"></i></a></li>
+                    <li><a href="#"id="pro4"><i class="fa fa-random"></i></a></li>
+                    <li><a href="#"id="pro4"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+                <h3 class="title"><a href="#"id="pro4">profetional 4 cv</a></h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-6 my-3">
+        <div class="product-grid">
+            <div class="product-image">
+                <a href="#" class="image" id="pro5">
+                    <img class="pic-1" src="upload-img/profetional2cv.jpeg">
+                    <img class="pic-2" src="upload-img/profetional2cv.jpeg">
+                </a>
+                <ul class="product-links">
+                    <li><a href="#"id="pro5">download</a></li>
+                    <li><a href="#"id="pro5"><i class="fa fa-heart"></i></a></li>
+                    <li><a href="#"id="pro5"><i class="fa fa-random"></i></a></li>
+                    <li><a href="#"id="pro5"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+            <h3 class="title"><a href="#pro5" id="pro5">profetional 2 cv</a></h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-6 my-3">
+        <div class="product-grid">
+            <div class="product-image">
+                <a href="#" class="image" id="pro1">
+                    <img class="pic-1" src="upload-img/profetionalcv.jpeg">
+                    <img class="pic-2" src="upload-img/profetionalcv.jpeg">
+                </a>
+                <ul class="product-links">
+                    <li><a href="#"id="pro1">download</a></li>
+                    <li><a href="#"id="pro1"><i class="fa fa-heart"></i></a></li>
+                    <li><a href="#"id="pro1"><i class="fa fa-random"></i></a></li>
+                    <li><a href="#"id="pro1"><i class="fa fa-eye"></i></a></li>
+                </ul>
+            </div>
+            <div class="product-content">
+                <h3 class="title"><a href="#"id="pro1">profetional</a></h3>
+            </div>
+        </div>
+    </div>
+</div>
+</div> 
+
+<script>
+    // load the creat cv form data function 
+function creatcvform(){
+  $.ajax({
+    url:"cv/index.php",
+    method: "POST",
+    success: function(response){
+      if(response==0){
+        $("#cv-body").html("");
+        $("#warrning-message").html("");
+        $("#registration").modal("hide");
+        $("#warrning").modal("show");
+        $("#warrning-message").append("you have to login and make cv");
+        setTimeout(function(){
+           $("#warrning").modal("hide");
+       }, 5000);
+       $("#warrning").on('hidden.bs.modal', function () {
+        login();
+       });
+       $("#log-in").on('hidden.bs.modal', function () {
+         creatcvform();
+       });
+       registration();
+      }else{
+        $("#cv-body").html(response);
+      }
+    }
+ }); 
+}
+
+
+// creative click btn cv
+$("#creative").click(function(){
+  loadcreativecv();
+});
+// creativecv load function or first cv
+function loadcreativecv(){
+  $.ajax({
+    url: "cv/creativecv.php",
+    method:"GET",
+    success:function(data){
+      if (data==0) {
+        $("#cv-body").html("");
+        $("#warrning-message").html("");
+        $("#registration").modal("hide");
+        $("#warrning").modal("show");
+        $("#warrning-message").append("you have to login and download your  cv");
+        setTimeout(function(){
+           $("#warrning").modal("hide");
+       }, 5000);
+       $("#warrning").on('hidden.bs.modal', function () {
+        login();
+       });
+       $("#log-in").on('hidden.bs.modal', function () {
+        loadcreativecv();
+       });
+       registration();
+      } else{
+        $("#cv-body").html(data);
+      } 
+      }  
+  });
+}
+
+// creative 2 click btn cv
+$("#creative2").click(function(){
+  loadcreative2cv();
+});
+// creativecv load function or first cv
+function loadcreative2cv(){
+  $.ajax({
+    url: "cv/creative2cv.php",
+    method:"GET",
+    success:function(data){
+      if (data==0) {
+        $("#cv-body").html("");
+        $("#warrning-message").html("");
+        $("#registration").modal("hide");
+        $("#warrning").modal("show");
+        $("#warrning-message").append("you have to login and download your  cv");
+        setTimeout(function(){
+           $("#warrning").modal("hide");
+       }, 5000);
+       $("#warrning").on('hidden.bs.modal', function () {
+        login();
+       });
+       $("#log-in").on('hidden.bs.modal', function () {
+        loadcreative2cv();
+       });
+       registration();
+      } else{
+        $("#cv-body").html(data);
+      } 
+      }  
+  });
+}
+// creative 3 click btn cv
+$("#creative3").click(function(){
+  loadcreative3cv();
+});
+// creativecv load function or first cv
+function loadcreative3cv(){
+  $.ajax({
+    url: "cv/creative3cv.php",
+    method:"GET",
+    success:function(data){
+      if (data==0) {
+        $("#cv-body").html("");
+        $("#warrning-message").html("");
+        $("#registration").modal("hide");
+        $("#warrning").modal("show");
+        $("#warrning-message").append("you have to login and download your  cv");
+        setTimeout(function(){
+           $("#warrning").modal("hide");
+       }, 5000);
+       $("#warrning").on('hidden.bs.modal', function () {
+        login();
+       });
+       $("#log-in").on('hidden.bs.modal', function () {
+        loadcreative3cv();
+       });
+       registration();
+      } else{
+        $("#cv-body").html(data);
+      } 
+      }  
+  });
+}
+
+// creative 3 click btn cv
+$("#info1").click(function(){
+  info1cv();
+});
+// creativecv load function or first cv
+function info1cv(){
+  $.ajax({
+    url: "cv/info1cv.php",
+    method:"GET",
+    success:function(data){
+      if (data==0) {
+        $("#cv-body").html("");
+        $("#warrning-message").html("");
+        $("#registration").modal("hide");
+        $("#warrning").modal("show");
+        $("#warrning-message").append("you have to login and download your  cv");
+        setTimeout(function(){
+           $("#warrning").modal("hide");
+       }, 5000);
+       $("#warrning").on('hidden.bs.modal', function () {
+        login();
+       });
+       $("#log-in").on('hidden.bs.modal', function () {
+        info1cv();
+       });
+       registration();
+      } else{
+        $("#cv-body").html(data);
+      } 
+      }  
+  });
+}
+// profetional 3 click btn cv
+$("#pro3").click(function(){
+  pro3cv();
+});
+function pro3cv(){
+  $.ajax({
+    url: "cv/pro3cv.php",
+    method:"GET",
+    success:function(data){
+      if (data==0) {
+        $("#cv-body").html("");
+        $("#warrning-message").html("");
+        $("#registration").modal("hide");
+        $("#warrning").modal("show");
+        $("#warrning-message").append("you have to login and download your  cv");
+        setTimeout(function(){
+           $("#warrning").modal("hide");
+       }, 5000);
+       $("#warrning").on('hidden.bs.modal', function () {
+        login();
+       });
+       $("#log-in").on('hidden.bs.modal', function () {
+        pro3cv();
+       });
+       registration();
+      } else{
+        $("#cv-body").html(data);
+      } 
+      }  
+  });
+}
+// profetional 4 click btn cv
+$("#pro4").click(function(){
+  pro4cv();
+});
+function pro4cv(){
+  $.ajax({
+    url: "cv/pro4cv.php",
+    method:"GET",
+    success:function(data){
+      if (data==0) {
+        $("#cv-body").html("");
+        $("#warrning-message").html("");
+        $("#registration").modal("hide");
+        $("#warrning").modal("show");
+        $("#warrning-message").append("you have to login and download your  cv");
+        setTimeout(function(){
+           $("#warrning").modal("hide");
+       }, 5000);
+       $("#warrning").on('hidden.bs.modal', function () {
+        login();
+       });
+       $("#log-in").on('hidden.bs.modal', function () {
+        pro4cv();
+       });
+       registration();
+      } else{
+        $("#cv-body").html(data);
+      } 
+      }  
+  });
+}
+// profetional 5 click btn cv
+$("#pro5").click(function(){
+  pro5cv();
+});
+function pro5cv(){
+  $.ajax({
+    url: "cv/profetional2cv.php",
+    method:"GET",
+    success:function(data){
+      if (data==0) {
+        $("#cv-body").html("");
+        $("#warrning-message").html("");
+        $("#registration").modal("hide");
+        $("#warrning").modal("show");
+        $("#warrning-message").append("you have to login and download your  cv");
+        setTimeout(function(){
+           $("#warrning").modal("hide");
+       }, 5000);
+       $("#warrning").on('hidden.bs.modal', function () {
+        login();
+       });
+       $("#log-in").on('hidden.bs.modal', function () {
+        pro5cv();
+       });
+       registration();
+      } else{
+        $("#cv-body").html(data);
+      } 
+      }  
+  });
+}
+// profetional  click btn cv
+$("#pro1").click(function(){
+  pro1cv();
+});
+function pro1cv(){
+  $.ajax({
+    url: "cv/profetionalcv.php",
+    method:"GET",
+    success:function(data){
+      if (data==0) {
+        $("#cv-body").html("");
+        $("#warrning-message").html("");
+        $("#registration").modal("hide");
+        $("#warrning").modal("show");
+        $("#warrning-message").append("you have to login and download your  cv");
+        setTimeout(function(){
+           $("#warrning").modal("hide");
+       }, 5000);
+       $("#warrning").on('hidden.bs.modal', function () {
+        login();
+       });
+       $("#log-in").on('hidden.bs.modal', function () {
+        pro1cv();
+       });
+       registration();
+      } else{
+        $("#cv-body").html(data);
+      } 
+      }  
+  });
+}
+</script>
+
+
+
+
+
+
